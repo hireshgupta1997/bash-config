84dad74d94d068fb75c31d087229aec8e5ccb197
Arneh Jain <arnjain@adobe.com>
2017-04-09 14:51:48 +0530

    Add bash_profile

---
+++ .bash_profile
@@ -0,0 +1,18 @@
+
+# Git Alias
+alias gs='git status'
+alias ga='git add'
+alias gl='git log --pretty=format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%C(cyan)\\ [%cn]" --decorate --date=short'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gds='git diff --staged'
+alias gc='git commit'
+alias gcm='git commit -m '
+alias grh='git reset HEAD'
+
+
+# Docker Commands
+alias dps='docker ps'
+alias dr='docker run'
+alias dkill='docker kill'
+alias drm='docker rm'
---
+++ scripts/tests/colors-test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Test file for colors.sh
+source '../colors.sh'
+
+MESSAGE='The quick brown fox jumps over the lazy dog.'
+
+
+echo -e $BLACK$MESSAGE
+echo -e $RED$MESSAGE
+echo -e $GREEN$MESSAGE
+echo -e $ORANGE$MESSAGE
+echo -e $BLUE$MESSAGE
+echo -e $PURPLE$MESSAGE
+echo -e $CYAN$MESSAGE
+echo -e $LGRAY$MESSAGE
+echo -e $DGRAY$MESSAGE
+echo -e $LRED$MESSAGE
+echo -e $LGREEN$MESSAGE
+echo -e $YELLOW$MESSAGE
+echo -e $LBLUE$MESSAGE
+echo -e $LPURPLE$MESSAGE
+echo -e $LCYAN$MESSAGE
+echo -e $WHITE$MESSAGE
+echo -e $NC$MESSAGE
