8dbcf509c3e7f20cf90e503b5cb5b459bf981377
Arneh Jain <jain.arneh@gmail.com>
2018-08-02 19:50:44 +0530

    Fix bugs in previous commit.
    Changes:
     - Also handle the case when Vundle is already installed

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ source 'scripts/colors.sh'
 
 STATUS=${ORANGE}
 SUCCESS=${LGREEN}
+WARN=${LCYAN}
+
+INSTALL_VUNDLE=${INSTALL_VUNDLE:-0}
 
 echo -e ${STATUS}'Running install.sh'${NC}
 
@@ -21,11 +24,21 @@ echo -e ${STATUS}'Copying completed.'${NC}
 echo -e ${SUCCESS}'Scripts Installation Complete.'${NC}
 
 VIM_PATH=$(which vim)
-if [ -n ${VIM_PATH} ]; then
+if [ ! -z ${VIM_PATH} ]; then
 	echo -e ${STATUS}'Detected an installed vim instance at ${VIM_PATH}'${NC}
 	read -p "Do you want to install the plugins (y/n)?" -n 1 -r
+	echo -e
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		echo -e ${STATUS}'Intalling vundle'${NC}
+		if [ -d ~/.vim/bundle/Vundle.vim ]; then
+			if [ "${INSTALL_VUNDLE}" -eq "1" ]; then
+				echo -e ${STATUS}'Removing existing Vundle'${NC}
+				rm -rf ~/.vim/bundle/Vundle.vim
+			else
+				echo -e ${WARN}'Vundle already exists ...'${NC}
+				echo -e ${WARN}'Run: INSTALL_VUNDLE=1 ./install.sh to re-install'${NC}
+			fi
+		fi
 		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 		echo -e ${STATUS}'Copyting .vimrc to ~/.vimrc'${NC}
 		cp -r .vimrc ~/.vimrc
